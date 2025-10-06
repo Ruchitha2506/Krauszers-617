2d05f500496248e0b0ca5e630018469b867f6a9d
Ruchitha Batchu <ruchithabatchu2506@gmail.com>
2025-10-06 19:35:40 -0400

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,8 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
+  base: '/krauszers-617/',  // 👈 add this line
+})
+
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
